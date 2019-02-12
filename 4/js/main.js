7c1ef004ecb1793eb0b0fd2a6d9cb08eb5a9d027
Andrei Yasinski <andrewtopol856@gmail.com>
2019-02-12 11:38:10 +0300

    remove spaces in task 4

--- 4/js/main.js
+++ 4/js/main.js
@@ -1,15 +1,11 @@
 let getDimensionalArray = (arr, lgth) => {
   if(lgth == 1) return arr;
   let array = arr;
-
   let result = [];
-
   while (array.length) {
     result.push(array.splice(0, lgth));
   }
-
   return result
-
 }
 
 console.log(getDimensionalArray([0, 1, 2, 3, 4, 5], 3));
